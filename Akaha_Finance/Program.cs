2691c653629c6b95147c67a009bf16719027270d
Azamat <azapro66@gmail.com>
2025-07-10 23:22:28 +0600

    Method for show finance

--- Akaha_Finance/Program.cs
+++ Akaha_Finance/Program.cs
@@ -6,7 +6,16 @@ namespace AkahaFinance
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+
             Console.WriteLine("Hello, World!");
+            decimal MyFinance = 0;
+            program.ShowMyFinance(MyFinance);
+        }
+
+        public void ShowMyFinance(decimal myFinance)
+        {
+            Console.WriteLine($"Мои финансы: {myFinance} сом");
         }
     }
 }
